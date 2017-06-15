404de86d06e3682496f913c2230dff8c239cb991
Jorge Silva <jsilvab.externo@bancofalabella.cl>
2017-06-15 15:10:15 -0400

    Archivo de parámetros globales creado. Se crea además el archivo que genera los Logs por consola.

--- src/vista/Buscador.java
+++ src/vista/Buscador.java
@@ -1,12 +1,15 @@
 package vista;
 
 import java.io.*;
+import modelo.Log;
+import modelo.Parametros;
 
 /**
  * Clase que lee de forma recursiva dentro de un directorio.
  * @author Jorge Silva Borda
  */
 public class Buscador {
+    Log log = Parametros.log;
 
     public void buscar(File arc) {
         if (arc.exists()) {
@@ -17,7 +20,7 @@ public class Buscador {
                         buscar(archivo);
                     } else {
                         //Aplicar acá la lógica del momento en que se da con un archivo.
-                        System.out.println(archivo.getAbsolutePath());
+                        log.log(archivo.getAbsolutePath());
                     }
                 }
             }
