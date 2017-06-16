7431c88750a46ee944bd33295c4b9b5cba314831
Jorge Silva <jsilvab.externo@bancofalabella.cl>
2017-06-16 14:58:29 -0400

    Creada clase ParseServer. Servirá para poder parsear los archivos que generan servidores Tuxedo.

---
+++ src/controlador/ParseServer.java
@@ -0,0 +1,10 @@
+package controlador;
+
+/**
+ * Clase para parsear los servidores Tuxedo y ubicar todas las
+ * funciones internas.
+ * @author Jorge Silva Borda
+ */
+public class ParseServer {
+    
+}
