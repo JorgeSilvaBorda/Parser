2ac46400a778368d2446d59be40d3f5f0dd6d5d0
Jorge Silva <jsilvab.externo@bancofalabella.cl>
2017-06-28 12:18:15 -0400

    Parser SQL extrae querys completas del texto.

--- src/controlador/ParserSQL.java
+++ src/controlador/ParserSQL.java
@@ -1,9 +1,65 @@
 package controlador;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Clase para parsear querys de SQL
+ *
  * @author Jorge Silva Borda
  */
 public class ParserSQL {
+
+    public ParserSQL() {
+
+    }
+
+    public void parsearVariable(String nodo) {
+        String patron = "(\")(select|insert|update|delete)(.*?)(;;*)";
+        String declarador = nodo.split("=")[0];
+        String variable = declarador.split(" ")[declarador.split(" ").length - 1];
+        if (variable.contains("*")) {
+            variable = variable.split("\\*")[1];
+        }
+        System.out.print("Variable: " + variable);
+        Pattern p = Pattern.compile(patron, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+        Matcher m = p.matcher(nodo);
+
+        while (m.find()) {
+            System.out.println(" Tiene query");
+            buscarTablas(m.group());
+        }
+    }
+
+    public void parsearFuncion(String nodo) {
+        //String patron = "(EXEC SQL)(\\s?)(SELECT|INSERT|UPDATE|DELETE)(.*?);;*";
+        String patron = "(SELECT|INSERT|UPDATE|DELETE)(.*?);;*";
+        nodo = emparejarTexto(nodo);
+        String declarador = nodo.split("->")[1];
+        String nombre = declarador.split("\\(")[0].trim().split(" ")[1];
+        System.out.print("Funcion: " + nombre);
+        Pattern p = Pattern.compile(patron, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+        Matcher m = p.matcher(nodo);
+        while (m.find()) {
+            System.out.println(" Tiene query");
+            buscarTablas(m.group());
+        }
+    }
     
+    public String emparejarTexto(String texto){
+        texto = texto.replaceAll("  ", " ");
+        texto = texto.replaceAll("\\\\", "");
+        texto = texto.replaceAll("\\n", "");
+        texto = texto.replaceAll("\\r", "");
+        return texto;
+    }
+
+    public void buscarTablas(String texto) {
+        String query = texto.substring(1, texto.length() - 2).trim();
+        query = emparejarTexto(query);
+        query = query + ";";
+
+        System.out.println(query);
+        System.out.println("Query: " + query);
+    }
 }
--- src/controlador/ParserServer.java
+++ src/controlador/ParserServer.java
@@ -10,7 +10,6 @@ import modelo.servidor.ServicioTuxedo;
 import modelo.servidor.ServidorTuxedo;
 import modelo.servidor.dependencia.Funcion;
 import org.eclipse.cdt.core.dom.ast.ASTVisitor;
-import org.eclipse.cdt.core.dom.ast.DOMException;
 import org.eclipse.cdt.core.dom.ast.ExpansionOverlapsBoundaryException;
 import org.eclipse.cdt.core.dom.ast.IASTAttribute;
 import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
@@ -70,15 +69,15 @@ public final class ParserServer {
             if ((declaration instanceof IASTSimpleDeclaration)) {
                 IASTSimpleDeclaration ast = (IASTSimpleDeclaration) declaration;
                 //try {
-                    //System.out.println(ast.getSyntax().toString() + " " + ast.getChildren().length);
-                    IASTNode typedef = ast.getChildren().length == 1 ? ast.getChildren()[0] : ast.getChildren()[1];
-                    //System.out.println(typedef);
-                    IASTNode[] children = typedef.getChildren();
-                    if ((children != null) && (children.length > 0)) {
-                        //System.out.println(children[0].getRawSignature());
-                    }
+                //System.out.println(ast.getSyntax().toString() + " " + ast.getChildren().length);
+                IASTNode typedef = ast.getChildren().length == 1 ? ast.getChildren()[0] : ast.getChildren()[1];
+                //System.out.println(typedef);
+                IASTNode[] children = typedef.getChildren();
+                if ((children != null) && (children.length > 0)) {
+                    //System.out.println(children[0].getRawSignature());
+                }
                 //} catch (ExpansionOverlapsBoundaryException e) {
-                    //System.out.println("Error: " + e);
+                //System.out.println("Error: " + e);
                 //}
 
                 IASTDeclarator[] declarators = ast.getDeclarators();
@@ -96,10 +95,10 @@ public final class ParserServer {
                 IASTFunctionDefinition ast = (IASTFunctionDefinition) declaration;
                 IScope scope = ast.getScope();
                 //try {
-                    //System.out.println(scope.getParent().getScopeName());
-                    //System.out.println(ast.getSyntax());
+                //System.out.println(scope.getParent().getScopeName());
+                //System.out.println(ast.getSyntax());
                 //} catch (DOMException | ExpansionOverlapsBoundaryException e) {
-                    //System.out.println("Error: " + e);
+                //System.out.println("Error: " + e);
                 //}
                 ICPPASTFunctionDeclarator typedef = (ICPPASTFunctionDeclarator) ast.getDeclarator();
                 //System.out.println(typedef.getName());
@@ -156,7 +155,6 @@ public final class ParserServer {
 
         //Obtener las funciones del código.
         //System.out.println("Archivo servidor: " + this.archivoFuente.getAbsolutePath());
-
         //Para el proceso de los archivos fuentes.
         for (File dependencia : dependencias) {
             System.out.println("Archivo examinado: " + dependencia.getAbsolutePath());
@@ -213,35 +211,65 @@ public final class ParserServer {
         }
 
         if (!node.getClass().getSimpleName().equals("CPPASTTranslationUnit")) {
-            if (node.getClass().getSimpleName().equals("CPPASTFunctionDeclarator")
-                    || node.getClass().getSimpleName().equals("CPPASTFunctionCallExpression")
-                    /*|| node.getClass().getSimpleName().equals("CPPASTName")*/) {
-                //System.out.print(index + ":");
-                //System.out.println(String.format(new StringBuilder("%1$").append(index * 2).append("s").toString(), new Object[]{"-"}) + node.getClass().getSimpleName() + offset + " -> " + (printContents ? node.getRawSignature().replaceAll("\n", " \\ ") : node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength())));
-                Object[] objeto = new Object[2];
-                switch (node.getClass().getSimpleName()) {
-                    case "CPPASTFunctionDeclarator":
-                        nombre = node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength()).toString().split("\\(")[0];
-                        f = new Funcion(nombre, "Declara");
-                        if(node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength()).toString().split("\\(")[1].contains("TPSVCINFO")){
-                            f.setEsServicio(true);
-                        }else{
-                            f.setEsServicio(false);
-                        }
-                        objeto[0] = f;
-                        objeto[1] = index;
+            //if (node.getClass().getSimpleName().equals("CPPASTFunctionDeclarator")
+            //|| node.getClass().getSimpleName().equals("CPPASTFunctionCallExpression")
+            /*|| node.getClass().getSimpleName().equals("CPPASTName")*///) {
+            //System.out.print(index + ":");
 
-                        objetos.add(objeto);
-                        break;
-                    case "CPPASTFunctionCallExpression":
-                        nombre = node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength()).toString().split("\\(")[0];
-                        f = new Funcion(nombre, "Llama");
-                        objeto[0] = f;
-                        objeto[1] = index;
-                        objetos.add(objeto);
-                        break;
-                }
+            Object[] objeto = new Object[2];
+            switch (node.getClass().getSimpleName()) {
+                case "CPPASTFunctionDeclarator":
+                    nombre = node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength()).toString().split("\\(")[0];
+                    f = new Funcion(nombre, "Declara");
+                    if (node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength()).toString().split("\\(")[1].contains("TPSVCINFO")) {
+                        f.setEsServicio(true);
+                    } else {
+                        f.setEsServicio(false);
+                    }
+                    objeto[0] = f;
+                    objeto[1] = index;
+
+                    objetos.add(objeto);
+                    break;
+                case "CPPASTFunctionCallExpression":
+                    nombre = node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength()).toString().split("\\(")[0];
+                    f = new Funcion(nombre, "Llama");
+                    objeto[0] = f;
+                    objeto[1] = index;
+                    objetos.add(objeto);
+                    break;
             }
+            String nodo = String.format(new StringBuilder("%1$").append(index * 2).append("s").toString(), new Object[]{"-"}) + node.getClass().getSimpleName() + offset + " -> " + (printContents ? node.getRawSignature().replaceAll("\n", " \\ ") : node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength()));
+            //System.out.println(String.format(new StringBuilder("%1$").append(index * 2).append("s").toString(), new Object[]{"-"}) + node.getClass().getSimpleName() + offset + " -> " + (printContents ? node.getRawSignature().replaceAll("\n", " \\ ") : node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength())));
+            if (nodo.toUpperCase().contains("SELECT")
+                    || nodo.toUpperCase().contains("UPDATE")
+                    || nodo.toUpperCase().contains("INSERT")
+                    || nodo.toUpperCase().contains("DELETE")) {
+                //System.out.println(String.format(new StringBuilder("%1$").append(index * 2).append("s").toString(), new Object[]{"-"}) + node.getClass().getSimpleName() + offset + " -> " + (printContents ? node.getRawSignature().replaceAll("\n", " \\ ") : node.getRawSignature().subSequence(0, node.getFileLocation().getNodeLength())));
+                //Para los sql.c
+                //CPPASTSimpleDeclaration contiene la declaración de una variable con query.
+                //Luego viene CPPASTDeclarator con el nombre de la variable (Puede contener asterisco delante de ella) [* nomVar =]
+                //Luego CPPASTDeclarator Contiene el String de la query
+                //Expresion para extraer el texto: (\")(select|insert|update|delete)(.*?)(;;*)
+                //CPPASTFunctionDefinition Indica que viene la declaración de una función con query. 
+                //CPPASTExpressionStatement Puede tratarse de una variable que contenga query
+                //CPPASTLiteralExpression contiene la cadena con query en caso de ser
+                ParserSQL parserSql = new ParserSQL();
+                if(node.getClass().getSimpleName().equals("CPPASTSimpleDeclaration") && nodo.contains(" char ")){
+                    //Es variable
+                    parserSql.parsearVariable(nodo);
+                }else if(node.getClass().getSimpleName().equals("CPPASTFunctionDefinition")){
+                    //Es funcion
+                    parserSql.parsearFuncion(nodo);
+                }
+                //Para los sql.pc
+                //CPPASTFunctionDefinition 
+                //CPPASTCompoundStatement contendría la query
+                //Las querys terminan con ;
+                //Expresion para extraer texto de las querys: (EXEC SQL)(\s?)(SELECT|INSERT|UPDATE|DELETE)(.*?);;*
+            }
+
+            //}
         }
 
         for (IASTNode iastNode : children) {
@@ -259,11 +287,11 @@ public final class ParserServer {
 
             for (int i = 1; i < objetos.size(); i++) {
                 if ((int) objetos.get(i)[1] > indice) {
-                    if(!funcs.contains((Funcion) objetos.get(i)[0])){
+                    if (!funcs.contains((Funcion) objetos.get(i)[0])) {
                         funcs.get(actual).addLlamado((Funcion) objetos.get(i)[0]);
                     }
                 } else {
-                    if(!funcs.contains((Funcion) objetos.get(i)[0])){
+                    if (!funcs.contains((Funcion) objetos.get(i)[0])) {
                         funcs.add((Funcion) objetos.get(i)[0]);
                         actual++;
                     }
@@ -273,54 +301,54 @@ public final class ParserServer {
         }
         return funcs;
     }
-    
-    public LinkedList<Funcion> armarFunciones2(LinkedList<Object[]> objetos){
+
+    public LinkedList<Funcion> armarFunciones2(LinkedList<Object[]> objetos) {
         int funActual = -1;
-        int index = (int)objetos.get(0)[1];
+        int index = (int) objetos.get(0)[1];
         LinkedList<Funcion> funcs = new LinkedList();
-        for(Object[] obj : objetos){
-            if(((Funcion)obj[0]).getTipo().equals("Declara")){
-                funcs.add((Funcion)obj[0]);
+        for (Object[] obj : objetos) {
+            if (((Funcion) obj[0]).getTipo().equals("Declara")) {
+                funcs.add((Funcion) obj[0]);
                 funActual++;
-            }else{
-                if(!new modelo.Utilidades().existeEnLista((Funcion)obj[0], funcs.get(funActual).getLlamados())){
-                    funcs.get(funActual).addLlamado((Funcion)obj[0]);
+            } else {
+                if (!new modelo.Utilidades().existeEnLista((Funcion) obj[0], funcs.get(funActual).getLlamados())) {
+                    funcs.get(funActual).addLlamado((Funcion) obj[0]);
                 }
             }
         }
         return funcs;
     }
-    
-    public ServidorTuxedo construirServidor(){
+
+    public ServidorTuxedo construirServidor() {
         ServidorTuxedo servidor = new ServidorTuxedo();
         servidor.setNombre(nomServer);
-        for(File dependencia : dependencias){
+        for (File dependencia : dependencias) {
             servidor.addDependencia(dependencia);
         }
-        for(Funcion f : funciones){
-            if(f.getEsServicio()){
+        for (Funcion f : funciones) {
+            if (f.getEsServicio()) {
                 ServicioTuxedo servicio = new ServicioTuxedo();
                 servicio.setNombre(f.getNombre());
-                
-                if(f.getLlamados().size() > 0){
-                    for(Funcion llam : f.getLlamados()){
-                        
-                        if(!new modelo.Utilidades().existeEnLista(llam, servicio.getFunciones())){
+
+                if (f.getLlamados().size() > 0) {
+                    for (Funcion llam : f.getLlamados()) {
+
+                        if (!new modelo.Utilidades().existeEnLista(llam, servicio.getFunciones())) {
                             servicio.addLlamado(llam);
                         }
                     }
                 }
-                if(!servidor.getServicios().contains(servicio)){
+                if (!servidor.getServicios().contains(servicio)) {
                     servidor.addServicio(servicio);
                 }
-                
-            }else{
-                if(!new modelo.Utilidades().existeEnLista(f, servidor.getFunciones())){
+
+            } else {
+                if (!new modelo.Utilidades().existeEnLista(f, servidor.getFunciones())) {
                     servidor.addFuncion(f);
                 }
             }
         }
-        
+
         return servidor;
     }
 
